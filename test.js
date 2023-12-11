fce4e353482b6f9e003d4042c906260dff69f713
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-11 17:16:40 +0100

    add 2 test loop

---
+++ test.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    break; // exit the loop when i is 5
+  }
+  console.log(i);
+}
+
---
+++ test2.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 5; i++) {
+  if (i === 2) {
+    continue; // skip the rest of the loop body when i is 2
+  }
+  console.log(i);
+}
+
